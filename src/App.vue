9be3e5ffb94a63f7d093ae447dbbf552edcdaa49
zhousg <zhoushugang@itcast.cn>
2020-02-06 10:44:58 +0800

    登录模块-路由规则和组件准备

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <!-- 空的App组件 -->
-    <el-button type="success">成功按钮</el-button>
+    <!-- 一级路由 -->
+    <router-view></router-view>
   </div>
 </template>
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class='container'>Login</div>
+</template>
+
+<script>
+export default {
+  name: 'app-login'
+}
+</script>
+
+<style scoped lang='less'></style>
