a88e17093cf380d4016ef0dc92fcf85bf0a29587
zhousg <zhoushugang@itcast.cn>
2020-02-06 14:57:48 +0800

    绘制表单

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,13 +4,39 @@
     <!-- 卡片 -->
     <el-card class="my-card">
       <img src="../../assets/logo_index.png" alt="">
+      <!-- 表单 -->
+      <el-form :model="loginForm">
+        <el-form-item>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:240px;margin-right:8px"></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <!-- :value="true" 选中复选框不能进行修改  必须同意 -->
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app-login'
+  name: 'app-login',
+  data () {
+    return {
+      // 表单对应的数据对象
+      loginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
 }
 </script>
 
