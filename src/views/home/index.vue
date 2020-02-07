48b8bee8d42d2a4f10209c3e1a437be73e2296cc
zhousg <zhoushugang@itcast.cn>
2020-02-07 11:00:56 +0800

    首页-基础布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class='container-home'>Home</div>
+  <el-container class="container-home">
+    <el-aside class="my-aside" width="200px">Aside</el-aside>
+    <el-container>
+      <el-header class="my-header">Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -8,4 +14,18 @@ export default {
 }
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.container-home {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  .my-aside{
+    background: #002233;
+  }
+  .my-header{
+    border-bottom:1px solid #ddd;
+  }
+}
+</style>
