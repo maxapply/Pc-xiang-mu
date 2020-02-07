81d9c63fa9025b7456bca32976bf9cdd4399d23d
zhousg <zhoushugang@itcast.cn>
2020-02-07 14:52:11 +0800

    首页-侧边栏

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,46 @@
 <template>
   <el-container class="container-home">
-    <el-aside class="my-aside" width="200px">Aside</el-aside>
+    <el-aside class="my-aside" width="200px">
+      <!-- logo区域 -->
+      <div class="logo"></div>
+      <!-- 导航菜单 -->
+      <el-menu
+        default-active="1"
+        background-color="#002233"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+        style="border-right:none"
+      >
+        <el-menu-item index="1">
+          <i class="el-icon-s-home"></i>
+          <span slot="title">首页</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-document"></i>
+          <span slot="title">內容管理</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-picture"></i>
+          <span slot="title">素材管理</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-s-promotion"></i>
+          <span slot="title">发布文章</span>
+        </el-menu-item>
+        <el-menu-item index="5">
+          <i class="el-icon-chat-dot-round"></i>
+          <span slot="title">评论管理</span>
+        </el-menu-item>
+        <el-menu-item index="6">
+          <i class="el-icon-present"></i>
+          <span slot="title">粉丝管理</span>
+        </el-menu-item>
+        <el-menu-item index="7">
+          <i class="el-icon-setting"></i>
+          <span slot="title">个人设置</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
     <el-container>
       <el-header class="my-header">
         <span class="icon el-icon-s-fold"></span>
@@ -9,7 +49,7 @@
         <el-dropdown class="my-dropdown">
           <span class="el-dropdown-link">
             <!-- 用户头像 -->
-            <img class="head" src="../../assets/avatar.jpg" alt="">
+            <img class="head" src="../../assets/avatar.jpg" alt />
             <!-- 用户名称 -->
             <strong class="name">周杰伦</strong>
             <i class="el-icon-arrow-down el-icon--right"></i>
@@ -40,6 +80,13 @@ export default {
   top: 0;
   .my-aside {
     background: #002233;
+    .logo {
+      width: 100%;
+      height: 60px;
+      // background: ...    背景图定位/背景图尺寸
+      background: #002244 url(../../assets/logo_admin.png) no-repeat center /
+        140px auto;
+    }
   }
   .my-header {
     border-bottom: 1px solid #ddd;
@@ -52,14 +99,14 @@ export default {
       vertical-align: middle;
       padding-left: 10px;
     }
-    .my-dropdown{
+    .my-dropdown {
       float: right;
       .head {
         width: 30px;
         height: 30px;
         vertical-align: middle;
       }
-      .name{
+      .name {
         padding-left: 5px;
         vertical-align: middle;
       }
