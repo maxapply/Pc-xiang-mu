4f56f25e88060e462a24ef6876b504f7c96b9153
zhousg <zhoushugang@itcast.cn>
2020-02-07 11:42:39 +0800

    首页-头部导航

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,7 +2,24 @@
   <el-container class="container-home">
     <el-aside class="my-aside" width="200px">Aside</el-aside>
     <el-container>
-      <el-header class="my-header">Header</el-header>
+      <el-header class="my-header">
+        <span class="icon el-icon-s-fold"></span>
+        <span class="text">江苏传智播客科技教育有限公司</span>
+        <!-- 右边下拉菜单 -->
+        <el-dropdown class="my-dropdown">
+          <span class="el-dropdown-link">
+            <!-- 用户头像 -->
+            <img class="head" src="../../assets/avatar.jpg" alt="">
+            <!-- 用户名称 -->
+            <strong class="name">周杰伦</strong>
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>个人设置</el-dropdown-item>
+            <el-dropdown-item>退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
       <el-main>Main</el-main>
     </el-container>
   </el-container>
@@ -21,11 +38,32 @@ export default {
   position: absolute;
   left: 0;
   top: 0;
-  .my-aside{
+  .my-aside {
     background: #002233;
   }
-  .my-header{
-    border-bottom:1px solid #ddd;
+  .my-header {
+    border-bottom: 1px solid #ddd;
+    line-height: 60px;
+    .icon {
+      font-size: 24px;
+      vertical-align: middle;
+    }
+    .text {
+      vertical-align: middle;
+      padding-left: 10px;
+    }
+    .my-dropdown{
+      float: right;
+      .head {
+        width: 30px;
+        height: 30px;
+        vertical-align: middle;
+      }
+      .name{
+        padding-left: 5px;
+        vertical-align: middle;
+      }
+    }
   }
 }
 </style>
