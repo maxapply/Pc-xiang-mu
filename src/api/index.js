23493f834ca4d3ac49cc4371cd4cc44f26b5d8c6
zhousg <zhoushugang@itcast.cn>
2020-02-10 14:48:28 +0800

    内容管理-axios的请求拦截器

--- src/api/index.js
+++ src/api/index.js
@@ -5,6 +5,14 @@ import auth from '@/utils/auth'
 // 基准地址配置
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
 // 请求头token配置
-axios.defaults.headers.Authorization = `Bearer ${auth.getUser().token}`
-
+// axios.defaults.headers.Authorization = `Bearer ${auth.getUser().token}`
+// 请求拦截器
+axios.interceptors.request.use(config => {
+  // 获取本地的token，如果有token咱们就追加即可
+  const user = auth.getUser()
+  if (user.token) config.headers.Authorization = `Bearer ${user.token}`
+  return config
+}, error => {
+  return Promise.reject(error)
+})
 export default axios
