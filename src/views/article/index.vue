e03d7730ebd823d350ab6e6133ef8788fd820ece
zhousg <zhoushugang@itcast.cn>
2020-02-11 11:03:58 +0800

    内容管理-筛选区域布局02-表单元素-频道日期按钮

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -12,7 +12,7 @@
         </el-breadcrumb>
       </div>
       <!-- 表单 -->
-      <el-form label-width="80px">
+      <el-form label-width="80px" size="small">
         <el-form-item label="状态：">
           <el-radio-group v-model="filterData.status">
             <el-radio :label="null">全部</el-radio>
@@ -23,9 +23,29 @@
             <el-radio :label="4">已删除</el-radio>
           </el-radio-group>
         </el-form-item>
-        <el-form-item label="频道："></el-form-item>
-        <el-form-item label="日期："></el-form-item>
-        <el-form-item></el-form-item>
+        <el-form-item label="频道：">
+          <el-select v-model="filterData.channel_id" placeholder="请选择">
+            <el-option
+              v-for="item in channelOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+            <!-- label 选项文字  value 选项的值 当你选择某个选项后，该选项的值提供v-model -->
+          </el-select>
+        </el-form-item>
+        <el-form-item label="日期：">
+          <el-date-picker
+            v-model="dateArr"
+            type="daterange"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+          ></el-date-picker>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">筛选</el-button>
+        </el-form-item>
       </el-form>
     </el-card>
     <!-- 筛选结果 -->
@@ -42,8 +62,19 @@ export default {
       // 筛选数据是由多个表单元素组成，需要收集所有数据，应该使用对象来进行绑定
       filterData: {
         // 当字段的值null，axios请求不会提交，代表不传
-        status: null
-      }
+        status: null,
+        channel_id: null,
+        begin_pubdate: null,
+        end_pubdate: null
+      },
+      // 频道下拉选项数据
+      channelOptions: [
+        { label: '前端', value: 1 },
+        { label: '数据库', value: 2 }
+      ],
+      // 日期范围数据 [起始日期,结束日期]
+      // 但是选择完成日期范围后，可以根据这个数据给 begin_pubdate end_pubdate 赋值。
+      dateArr: []
     }
   }
 }
