1a7e8115085e978bd04ee35b31cd6feebb2551a9
zhousg <zhoushugang@itcast.cn>
2020-02-14 14:47:47 +0800

    发布文章-基础布局

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,10 +1,36 @@
 <template>
-  <div class='container-publish'>Publish</div>
+  <div class='container-publish'>
+    <el-card>
+      <div slot="header">
+        <my-bread>发布文章</my-bread>
+      </div>
+      <el-form label-width="120px">
+        <el-form-item label="标题：">
+          <el-input v-model="articleForm.title" style="width:400px"></el-input>
+        </el-form-item>
+        <el-form-item label="内容：">富文本</el-form-item>
+        <el-form-item label="封面：">封面组件</el-form-item>
+        <el-form-item label="频道：">频道组件</el-form-item>
+        <el-form-item>
+          <el-button type="primary">发布文章</el-button>
+          <el-button>存入草稿</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'app-publish'
+  name: 'app-publish',
+  data () {
+    return {
+      // 文章数据
+      articleForm: {
+        title: null
+      }
+    }
+  }
 }
 </script>
 
