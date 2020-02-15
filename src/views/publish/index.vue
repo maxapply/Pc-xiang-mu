e127b3aac80f3eb7dbfa999657b0380881af57f3
zhousg <zhoushugang@itcast.cn>
2020-02-15 14:48:21 +0800

    发布文章-使用富文本

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -8,7 +8,9 @@
         <el-form-item label="标题：">
           <el-input v-model="articleForm.title" style="width:400px"></el-input>
         </el-form-item>
-        <el-form-item label="内容：">富文本</el-form-item>
+        <el-form-item label="内容：">
+          <quill-editor v-model="articleForm.content"></quill-editor>
+        </el-form-item>
         <el-form-item label="封面：">封面组件</el-form-item>
         <el-form-item label="频道：">
           <my-channel v-model="articleForm.channel_id"></my-channel>
@@ -19,24 +21,27 @@
         </el-form-item>
       </el-form>
     </el-card>
-    <!-- 测试组件 -->
-    <!-- :value="count" @input="count=$event"  就是v-model的语法糖底层 -->
-    <my-increment v-model="count"></my-increment>
   </div>
 </template>
 
 <script>
-import MyIncrement from '@/components/my-increment'
+// 富文本需要的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+// 组件配置对象
+import { quillEditor } from 'vue-quill-editor'
 export default {
-  components: { MyIncrement },
   name: 'app-publish',
+  components: { quillEditor },
   data () {
     return {
       count: 10,
       // 文章数据
       articleForm: {
         title: null,
-        channel_id: null
+        channel_id: null,
+        content: null
       }
     }
   }
