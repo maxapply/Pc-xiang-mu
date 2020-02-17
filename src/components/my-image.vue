cd594b30bf6b27181b9ff5e39985570afc6a3e35
zhousg <zhoushugang@itcast.cn>
2020-02-17 10:14:36 +0800

    发布文章-封面组件-素材列表基础布局

--- src/components/my-image.vue
+++ src/components/my-image.vue
@@ -7,7 +7,21 @@
     <!-- 对话框 -->
     <el-dialog :visible.sync="dialogVisible" width="750px">
       <el-tabs v-model="activeName" type="card">
-        <el-tab-pane label="素材库" name="list">1</el-tab-pane>
+        <el-tab-pane label="素材库" name="list">
+          <!-- 按钮 -->
+          <el-radio-group v-model="reqParams.collect" size="small">
+            <el-radio-button :label="false">全部</el-radio-button>
+            <el-radio-button :label="true">收藏</el-radio-button>
+          </el-radio-group>
+          <!-- 列表 -->
+          <div class="img-list">
+            <div class="img-item" v-for="i in 8" :key="i">
+              <img src="../assets/avatar.jpg" alt />
+            </div>
+          </div>
+          <!-- 分页 -->
+          <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+        </el-tab-pane>
         <el-tab-pane label="上传图片" name="upload">2</el-tab-pane>
       </el-tabs>
       <span slot="footer" class="dialog-footer">
@@ -23,6 +37,12 @@ export default {
   name: 'my-image',
   data () {
     return {
+      // 获取素材列表参数对象
+      reqParams: {
+        collect: false,
+        page: 1,
+        per_page: 8
+      },
       dialogVisible: false,
       activeName: 'list'
     }
@@ -36,10 +56,28 @@ export default {
 </script>
 
 <style scoped lang='less'>
+// 素材列表
+.img-list {
+  padding-top: 20px;
+  .img-item {
+    width: 150px;
+    height: 120px;
+    border: 1px dashed #ddd;
+    display: inline-block;
+    margin-right: 20px;
+    img {
+      width: 100%;
+      height: 100%;
+      display: block;
+    }
+  }
+}
+// 组件容器
 .my-image {
   display: inline-block;
   margin-right: 20px;
 }
+// 图片按钮
 .img_btn {
   width: 150px;
   height: 150px;
