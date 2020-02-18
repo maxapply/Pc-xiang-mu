8b4873a66973d1de748f8a55d3269474122306ff
zhousg <zhoushugang@itcast.cn>
2020-02-18 14:31:12 +0800

    评论管理-基础布局

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,12 +1,37 @@
 <template>
   <div class='container-comment'>
-    Comment
+    <el-card>
+      <div slot="header">
+        <my-bread>评论管理</my-bread>
+      </div>
+      <!-- 表格 -->
+      <el-table :data="comments">
+        <el-table-column label="文章标题"></el-table-column>
+        <el-table-column label="总评论数"></el-table-column>
+        <el-table-column label="粉丝评论数"></el-table-column>
+        <el-table-column label="评论状态"></el-table-column>
+        <el-table-column label="操作"></el-table-column>
+      </el-table>
+      <!-- 分页 -->
+      <el-pagination
+        style="margin-top:20px"
+        background
+        layout="prev, pager, next"
+        :total="1000">
+      </el-pagination>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app-comment'
+  name: 'app-comment',
+  data () {
+    return {
+      // 评论列表（文章列表，此时用来维护评论功能）
+      comments: []
+    }
+  }
 }
 </script>
 
