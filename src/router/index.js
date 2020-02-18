feefc7b16f65fa2102272886b8dd91b7fbcb3d8f
zhousg <zhoushugang@itcast.cn>
2020-02-18 14:20:37 +0800

    评论管理-组件与路由

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,7 @@ import NotFound from '@/views/404'
 import Article from '@/views/article'
 import Image from '@/views/image'
 import Publish from '@/views/publish'
+import Comment from '@/views/comment'
 
 Vue.use(VueRouter)
 // 初始化
@@ -28,7 +29,8 @@ const router = new VueRouter({
         { path: '/', component: Welcome },
         { path: '/article', component: Article },
         { path: '/image', component: Image },
-        { path: '/publish', component: Publish }
+        { path: '/publish', component: Publish },
+        { path: '/comment', component: Comment }
       ]
     },
     // 通配规则（以上所有的规则不符合的时候，走这个规则）
---
+++ src/views/comment/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class='container-comment'>
+    Comment
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app-comment'
+}
+</script>
+
+<style scoped lang='less'></style>
