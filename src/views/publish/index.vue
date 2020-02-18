7610e6dcd60b4563fb48476421e10a7bd6373589
zhousg <zhoushugang@itcast.cn>
2020-02-18 10:13:26 +0800

    发布文章-封面组件-发布文章

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -30,8 +30,8 @@
           <my-channel v-model="articleForm.channel_id"></my-channel>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary">发布文章</el-button>
-          <el-button>存入草稿</el-button>
+          <el-button @click="submit(false)" type="primary">发布文章</el-button>
+          <el-button @click="submit(true)">存入草稿</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -77,6 +77,21 @@ export default {
             ['image']
           ]
         }
+      }
+    }
+  },
+  methods: {
+    // 添加文章 draft===false 发布文章  draft===true  存入草稿
+    async submit (draft) {
+      try {
+        // 请求
+        await this.$http.post(`articles?draft=${draft}`, this.articleForm)
+        // 提示
+        this.$message.success(draft ? '存入草稿成功' : '发布文章成功')
+        // 跳转
+        this.$router.push('/article')
+      } catch (e) {
+        this.$message.error('操作失败')
       }
     }
   }
